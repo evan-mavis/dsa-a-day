37b0e89ff4051cc892db29fd368f070b77a63ee3
evan mavis <evanmavis3@gmail.com>
2025-09-04 22:07:42 -0400

    add bubble sort

---
+++ problems/udemy/bubble-sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+    return arr
+
+print(bubble_sort([4,2,6,5,1,3]))
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
