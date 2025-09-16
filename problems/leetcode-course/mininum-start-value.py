360de7b6a4ad6b429044381b3c118d3931ca0f21
evan mavis <evanmavis3@gmail.com>
2025-09-16 19:38:34 -0400

    corret min start value

--- problems/leetcode-course/mininum-start-value.py
+++ problems/leetcode-course/mininum-start-value.py
@@ -1,9 +1,10 @@
 def minStartValue(self, nums: List[int]) -> int:
-    prefix = [nums[0]]
-    for i in range(1, len(nums)):
-        prefix.append(nums[i] + prefix[-1])
-    if prefix[-1] < 0:
-        return -prefix[-1]  
+        minValue = 0
+        total = 0
 
-    return 1
+        for num in nums:
+            total += num
+            minValue = min(total, minValue)
+        
+        return 1 - minValue
 
