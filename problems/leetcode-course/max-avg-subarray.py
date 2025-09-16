832993368d3df470084c23681932d9c9a5c246ca
evan mavis <evanmavis3@gmail.com>
2025-09-16 19:04:19 -0400

    Add running sum 1d

--- problems/leetcode-course/max-avg-subarray.py
+++ problems/leetcode-course/max-avg-subarray.py
@@ -1,2 +1,22 @@
 def findMaxAverage(nums: List[int], k: int) -> float:
+    sum = 0
+
+    for j in range(k):
+        sum += nums[j]
+
+    avg = sum / k
+
+    for i in range(k, len(nums)):
+        sum += nums[i]
+        sum -= nums[i - k]
+
+        avg = max(avg, sum / k) 
+
+    return avg
+
+        
+
+
+    
+        
 
--- problems/leetcode-course/sorted-square.py
+++ problems/leetcode-course/sorted-square.py
@@ -1,8 +1,17 @@
-def sortedSquares(nums: List[int]) -> List[int]:
+def sortedSquares(self, nums: List[int]) -> List[int]:
+    n = len(nums)
+    result = [0] * n
     left = 0
-    right = len(nums) - 1 
+    right = n - 1
+    for i in range(n - 1, -1, -1):
+        if abs(nums[left]) < abs(nums[right]):
+            square = nums[right]
+            right -= 1
+        else:
+            square = nums[left]
+            left += 1
+        result[i] = square * square
+    return result 
 
-    ans = []
-    while left < right:
 
         
