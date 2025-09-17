be69bc5093d94e99a2087ffaefdc57c784e32d85
evan mavis <evanmavis3@gmail.com>
2025-09-17 17:09:55 -0400

    Add find highest altitude

---
+++ problems/easy/1732-find-the-highest-altitude.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        current_alt = 0
+        max_alt = 0
+        for i in range(len(gain)):
+            current_alt += gain[i]
+
+            max_alt = max(max_alt, current_alt)
+
+        return max_alt
