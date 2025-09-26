472951759b8a22f6565d0b5ece062084e35d568e
evan mavis <evanmavis3@gmail.com>
2025-09-26 14:03:10 -0400

    Add jewels and stones

---
+++ problems/easy/771-jewels-and-stones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_set = set(jewels)
+        
+        jewels_count = 0
+        for stone in stones:
+            if stone in jewels_set:
+                jewels_count += 1
+
+        return jewels_count
+            
+        
+        
