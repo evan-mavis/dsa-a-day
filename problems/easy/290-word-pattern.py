6fb4aa4d7462235e709f268b75c9a5f0ed773781
evan mavis <evanmavis3@gmail.com>
2025-09-27 20:22:05 -0400

    Add word pattern

---
+++ problems/easy/290-word-pattern.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s_arr = s.split(' ')
+
+        if len(s_arr) != len(pattern):
+            return False
+
+        s_map = {}
+        p_map = {}
+
+        for i in range(len(s)):
+            if s_arr[i] not in s_map:
+                s_map[s_arr[i]] = i 
+            if pattern[i] not in p_map:
+                p_map[pattern[i]] = i
+                
+            if p_map[pattern[i]] != s_map[s_arr[i]]:
+                return False
+            
+        return True
+                
+        
+        
