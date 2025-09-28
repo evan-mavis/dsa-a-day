a5618ab13518c98f8e69381174e1c14666b62e13
evan mavis <evanmavis3@gmail.com>
2025-09-28 10:00:27 -0400

    Optimize custom sort string

--- problems/medium/791-custom-sort-string.py
+++ problems/medium/791-custom-sort-string.py
@@ -1,28 +1,14 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        order_hm = {}
-       
-        for i in range(len(order)):
-           order_hm[i] = order[i] 
-
-        count_hm = {}
+        s_count = {}
         for i in range(len(s)):
-            count_hm[s[i]] = count_hm.get(s[i], 0) + 1
+            s_count[s[i]] = s_count.get(s[i], 0) + 1
 
         result_arr = []
-        for char in order_hm.values():
-            if char in count_hm:
-                while count_hm[char] > 0:
-                    result_arr.append(char)
-                    count_hm[char] = count_hm[char] - 1
-                    if count_hm[char] < 0:
-                        count_hm.pop(char)
+        for char in order:
+            result_arr.append(char * s_count.pop(char, 0))
 
-        for char in count_hm.keys():
-            while count_hm[char] > 0:
-                result_arr.append(char)
-                count_hm[char] = count_hm[char] - 1
-                if count_hm[char] < 0:
-                    count_hm.pop(char)
+        for char, freq in s_count.items():
+            result_arr.append(char * freq)
 
         return "".join(result_arr)
